3e9793ec4f42ed5aa367622b4d99127bb4e4bde4
notcelis <isaac.celis.3c@gmail.com>
2021-04-11 21:22:34 -0500

    v1.0

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ app = Flask(__name__)
 def welcome():
 
     if request.method == 'GET':
-        imgs = getimages(3)
-        sizes = get_size(imgs)
-        
+        imgs = []
+        sizes = []
         return render_template(
             'template.html',
             title = 'examen',
@@ -27,33 +26,24 @@ def welcome():
 @app.route('/imgs',methods=['POST','GET'])
 def imgs():
     if request.method == 'POST':
-        number_of_imgs = int(request.values.get('numero'))
-        if(number_of_imgs >10):
-            number_of_imgs = 10
-        imgs = getimages(number_of_imgs)
-        sizes = get_size(imgs)
+        imgs = getimages()
         return json.dumps(imgs)
         
-def getimages(number):
+def getimages():
     url = 'https://picsum.photos/'
     imgs = []
-    for i in range(number):
-        width = str(randint(200,1200))
-        height = str(randint(200,1200))
+    width = str(randint(350,1200))
+    height = str(randint(350,450))
 
-        random_img = 'https://picsum.photos/' + width + '/' + height
-        r = requests.get(
-            random_img,
-            stream = True
-        )
-        imgs.append(r.url)
+    random_img = 'https://picsum.photos/' + width + '/' + height
+    r = requests.get(
+        random_img,
+        stream = True
+    )
+    imgs.append(r.url)
     return imgs
 
-def get_size(imgs):
-    sizes = []
-    for i in imgs:
-        size = i.split('.')[2]
-        sizes.append('Ancho:'+size.split('/')[3]+' Alto:'+size.split('/')[4])
-    return sizes
+
+    return json.dumps(sizes)
 if __name__ == '__main__':
     app.run(debug=True,port=1998)
--- static/js/code.js
+++ static/js/code.js
@@ -1,9 +1,11 @@
 function get_sizes(img){
-    let size = img.split('.')[2];
-    size = 'Ancho:'+size.split('/')[3]+' Alto:' + size.split('/')[4];
+    let ref = img.split('.')[2];
+    let size = []
+    size.push(parseInt(ref.split('/')[3]));
+    size.push(parseInt(ref.split('/')[4]));
     return size;
 }
-function change_slide(obj, imgItems, imgPos, slider){
+function change_slide(obj, imgItems, imgPos){
     let direction = 1;
     
     if($(obj).parent().attr('class') == 'left'){
@@ -24,13 +26,12 @@ function change_slide(obj, imgItems, imgPos, slider){
                 imgPos--;
             }
     }
-    slider.hide();
+    $('.slider li').hide();
     $('.slider li:nth-child('+ imgPos +')').fadeIn(); 
     return imgPos;
 }
 
 $(document).ready(function(){
-
     /*
     *********************
             Slider
@@ -39,9 +40,7 @@ $(document).ready(function(){
     var imgItems = $('input#number').val(); 
     var imgPos = 1;
     $('div.left').hide();
-    var slides =  $('.slider li');
-    slides.hide(); 
-    $('.slider li:first').show();
+   
     
     $('span').click(function(){
         imgPos = change_slide(
@@ -51,6 +50,7 @@ $(document).ready(function(){
             slides,
         );
         if(imgPos == imgItems){
+            
             $('div.right').hide();
         }else{
             $('div.right').show();
@@ -72,75 +72,148 @@ $(document).ready(function(){
             Library
     *********************
     */
-
+    let animations = [
+        "data-aos='zoom-in'",
+        "data-aos='zoom-in-up'",
+        "data-aos='zoom-in-down'",
+        "data-aos='zoom-in-left'",
+        "data-aos='zoom-in-right'",
+        "data-aos='zoom-out'",
+        "data-aos='zoom-out-up'",
+        "data-aos='zoom-out-down'",
+        "data-aos='zoom-out-left'",
+        "data-aos='zoom-out-right'"
+    ]
+    let random_animation = [];
     $('input#number').change(function(){
-        imgItems = $(this).val()
+        
+        imgItems = $(this).val();
+        imgPos = 1;
         slides =  $('.slider li');
-        slides.empty();
+        $('ul.slider').empty();
 
         $('#imgs').empty();        
         let num = $(this).val();
-        if(num >10){
-            num = 10
-            $(this).val(10);
+        if(num >20){
+            num = 20
+            $(this).val(20);
         }
-        $.ajax({
-            url: '/imgs',
-            async: false,
-            type: 'POST',
-            data:{
-                numero : num
-              },
-            dataType: 'json',
-            success: function(response){
+        for(let i = 0; i < num; i++){
+            random_animation.push(animations[Math.floor((Math.random() * 10) + 1) - 1]);
+        }
 
-                let animations = [
-                    "data-aos='zoom-in'",
-                    "data-aos='zoom-in-up'",
-                    "data-aos='zoom-in-down'",
-                    "data-aos='zoom-in-left'",
-                    "data-aos='zoom-in-right'",
-                    "data-aos='zoom-out'",
-                    "data-aos='zoom-out-up'",
-                    "data-aos='zoom-out-down'",
-                    "data-aos='zoom-out-left'",
-                    "data-aos='zoom-out-right'"
-                ]
-               for(let i = 0; i<response.length; i++){
-                    url = response[i];
-                    sizes = get_sizes(url);
-                    random_animation = animations[Math.floor((Math.random() * 10) + 1) - 1]
-                    $('#imgs').append(`
-                        <div id='box' class="d-inline-block" `+ random_animation +`>
-                            <a href='`+ url +`'>
-                                <img style='border: 5px solid black;' src='`+ url +`' loading='lazy'>
-                            </a>
-                            <div class="container" style="background-color: black; height: 30px;">
-                                <p class="font-weight-normal text-white text-center">
-                                    `+ sizes+`
-                                </p>
-                            </div>
-                        </div>
-                        `);
-                    let this_slide =  $('.slider li').eq(i);
-                    this_slide.append(`
-                        <img src='`+ url +`' alt=''>
-                        `);
+        for(let i = 0; i < num; i++){
+
+            $.ajax({
+                url: '/imgs',
+                async: false,
+                type: 'POST',
+                dataType: 'json',
+                success: function(response){
+                    url = response[0];
+                },
+                error: function(error){
+                    console.log('not ok', error);
                 }
-            },
-            error: function(error){
-                console.log('not ok', error);
+            });
 
+            sizes = get_sizes(url);
+            ancho = sizes[0];
+            alto  = sizes[1];
+            $('#imgs').append(`
+                <div id='box' class="d-block"  `+ random_animation[i] +`>
+                    <div id="box2">
+                        
+                            <img src="`+ url +`" loading="lazy">
+                        
+                        
+                    </div>
+                    <div class="container" id="sizes" style="background-color: black; height: 30px;">
+                        <p class="font-weight-normal text-white text-center">
+                        Ancho:` + ancho + ` Alto:` + alto + `
+                        </p>
+                    </div>
+                </div>
+                <div id="myModal" class="modal">
+                    <span class="close">&times;</span>
+                    <img class="modal-content" id="img01">
+                    <div id="caption"></div>
+                </div>
+                `);
+            
+            if(ancho > alto){
+                if(alto < 350){
+                    $('img').last().css('heigth','350px');
+                    $('img').last().css('width','auto');
+                }else{
+                    $('img').last().css('width','auto');
+                    $('img').last().css('height','100%');
+                }
+            }else{//ancho < alto
+                if(ancho < 350){//ancho < 350 --> 350< alto
+                    $('img').last().css('width','350px');
+                    $('img').last().css('height','auto');
+                }else{//ancho > 350
+                    $('img').last().css('width','100%');
+                    $('img').last().css('height','auto');
+                }
             }
-        });
+
+            $('ul.slider').append(`
+                <li>
+                </li>
+            `);
+            let this_slide = $('.slider li').eq(i);
+            this_slide.append(`
+                <img src='`+ url +`' alt=''>
+            `);
+        }
+        $('.slider li').hide(); 
+        $('.slider li:first').show();
+        
     });
+    
 
     $('#refresh').click(function(){
         $('input#number').trigger('change');
     });
+    $('input#number').trigger('change');
+    var slides =  $('.slider li');
+    slides.hide(); 
+    $('.slider li:first').show();
+
+    setInterval(function(){
+        $('span')[1].trigger('click');
+        }, 10000);
     /*
     *********************
             Library
     *********************
+
+
+
+
     */
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    $('#box img').click(function(){
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = $(this);
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+
+    });
+   
+   
 });
